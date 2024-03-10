04ece94ac468e567f52248a385c8e4dd56dbafc1
Nadir Ishiguro <nadir-yttrium@users.noreply.github.com>
2024-03-10 14:31:15 +0100

    push nix version to 2.18

--- flake.nix
+++ flake.nix
@@ -5,63 +5,69 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = { nixpkgs, flake-parts, ... }@inputs: flake-parts.lib.mkFlake { inherit inputs; } {
-    imports = [
-      inputs.flake-parts.flakeModules.easyOverlay
-    ];
-    perSystem = { config, self', inputs', pkgs, system, ... }:
-      with pkgs;
-      let
-        nix = nixVersions.nix_2_16;
-        llvmPackages = llvmPackages_16;
-        nixd = callPackage ./default.nix {
-          inherit nix;
-          inherit llvmPackages;
-        };
-        nixdLLVM = nixd.override {
-          stdenv = if stdenv.isDarwin then stdenv else llvmPackages.stdenv;
-        };
-        regressionDeps = with pkgs; [
-          clang-tools
-          nixpkgs-fmt
-          valgrind
-        ];
-        shellOverride = old: {
-          nativeBuildInputs = old.nativeBuildInputs ++ regressionDeps;
-          shellHook = ''
-            export PATH="${pkgs.clang-tools}/bin:$PATH"
-            export NIX_DEBUG_INFO_DIRS=${nix.debug}/lib/debug
-            export NIX_SRC=${nix.src}
-          '';
-          hardeningDisable = [ "fortify" ];
-        };
-      in
-      {
-        packages.default = nixd;
-        overlayAttrs = {
-          inherit (config.packages) nixd;
-        };
-        packages.nixd = nixd;
-
-        devShells.llvm = nixdLLVM.overrideAttrs shellOverride;
-
-        devShells.default = nixd.overrideAttrs shellOverride;
-
-        devShells.nvim = pkgs.mkShell {
-          nativeBuildInputs = [
-            nixd
-            pkgs.nixpkgs-fmt
-            (import ./nixd/docs/editors/nvim-lsp.nix { inherit pkgs; })
+  outputs =
+    { nixpkgs, flake-parts, ... }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ inputs.flake-parts.flakeModules.easyOverlay ];
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          ...
+        }:
+        with pkgs;
+        let
+          nix = nixVersions.nix_2_18;
+          llvmPackages = llvmPackages_16;
+          nixd = callPackage ./default.nix {
+            inherit nix;
+            inherit llvmPackages;
+          };
+          nixdLLVM = nixd.override { stdenv = if stdenv.isDarwin then stdenv else llvmPackages.stdenv; };
+          regressionDeps = with pkgs; [
+            clang-tools
+            nixpkgs-fmt
+            valgrind
           ];
+          shellOverride = old: {
+            nativeBuildInputs = old.nativeBuildInputs ++ regressionDeps;
+            shellHook = ''
+              export PATH="${pkgs.clang-tools}/bin:$PATH"
+              export NIX_DEBUG_INFO_DIRS=${nix.debug}/lib/debug
+              export NIX_SRC=${nix.src}
+            '';
+            hardeningDisable = [ "fortify" ];
+          };
+        in
+        {
+          packages.default = nixd;
+          overlayAttrs = {
+            inherit (config.packages) nixd;
+          };
+          packages.nixd = nixd;
+
+          devShells.llvm = nixdLLVM.overrideAttrs shellOverride;
+
+          devShells.default = nixd.overrideAttrs shellOverride;
+
+          devShells.nvim = pkgs.mkShell {
+            nativeBuildInputs = [
+              nixd
+              pkgs.nixpkgs-fmt
+              (import ./nixd/docs/editors/nvim-lsp.nix { inherit pkgs; })
+            ];
+          };
+          devShells.vscodium = pkgs.mkShell {
+            nativeBuildInputs = [
+              nixd
+              pkgs.nixpkgs-fmt
+              (import ./nixd/docs/editors/vscodium.nix { inherit pkgs; })
+            ];
+          };
         };
-        devShells.vscodium = pkgs.mkShell {
-          nativeBuildInputs = [
-            nixd
-            pkgs.nixpkgs-fmt
-            (import ./nixd/docs/editors/vscodium.nix { inherit pkgs; })
-          ];
-        };
-      };
-    systems = nixpkgs.lib.systems.flakeExposed;
-  };
+      systems = nixpkgs.lib.systems.flakeExposed;
+    };
 }
